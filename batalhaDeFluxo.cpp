30459524127126be236581ad4388cce4ec440750
Andre dos Santos <43791601+Aandre99@users.noreply.github.com>
2020-01-31 00:01:59 -0300

    Add files via upload

--- batalhaDeFluxo.cpp
+++ batalhaDeFluxo.cpp
@@ -3,120 +3,112 @@
 #include <vector>
 #include <stdlib.h>
 #include <utility>
-#include <limits.h>
 
 #define MAX(x,y) (x > y) ? (x):(y)
 
 using namespace std; 
 
-int* visited;
-vector<pair<int,pair<int,int>>> components;
+vector<pair<int,pair<int,int>>> Comp;
 typedef vector<int> vi;
-typedef vector<vi> Adj;
+typedef vector<vi> Graph;
 
-int cont_pd = 0, cont_prism = 0, money_comp, T=0;
-int Memo[10000][10000];
+int* price_suborn = NULL;
+int* visited = NULL;
+int pd_cont = 0, prism_cont = 0, value_component = 0;
 
-void dfs(Adj g, int u, int size_pd, int* money_pd, int* money_prism){
+void dfs(Graph graph, int size_pd, int u){
 
 	int i;
-
-	if(u < size_pd){
-		money_comp += money_pd[u];
-		cont_pd+=1;
-	}
-	else{
-		money_comp += money_prism[u-2];
-		cont_prism+=1;
-	}
-
 	visited[u] = 1;
+
+	value_component += price_suborn[u-1];
+
+	if(u <= size_pd){
+		pd_cont+=1;
+	}else{
+		prism_cont+=1;
+	}
 	
-	for (i = 0; i < g[u].size(); i++)
+	for (i = 0; i < graph[u].size(); i++)
 	{
-		if(!visited[g[u][i]]){
-			dfs(g,g[u][i],size_pd,money_pd,money_prism);
+		if(!visited[graph[u][i]]){
+			dfs(graph,size_pd,graph[u][i]);
 		}
 	}
 }
-
-void m(int money){
-
-	int i,j;
-
-	for(i=0;i<components.size();++i)
-	{
-		for(j=0;j<money+1;j++){
-			cout << Memo[i][j] << " ";
-		}
-		cout << endl;
+int dp(int money, int i, int p, int Memo[][10001])
+{
+	cout << money << i << endl;
+	if(i == Comp.size() || money == 0){
+		Memo[i][money] = 0;
 	}
-	cout << endl;
-}
-int dp(int money, int i, int p){
-	
-	if(i == components.size()){
-		return 0;
-	}
-	if(Memo[i][money] != -1){
-		cout << "USando dp..." << endl;
+	else if(Memo[i][money] != -1){
 		return Memo[i][money];
 	}
-	else{
-		if(components[i].first > money){
-			Memo[i][money] = dp(money,i+1,p);
-		}
-		else{
-			if(p)
-				Memo[i][money] = MAX(components[i].second.second + dp(money-components[i].first,i+1,p),dp(money,i+1,p));
-			else
-				Memo[i][money] = MAX(components[i].second.first + dp(money-components[i].first,i+1,p),dp(money,i+1,p));
+	else if(Comp[i].first > money){
+		Memo[i][money] = dp(money,i+1,p,Memo);
+	}else{
+
+		if(p){
+			Memo[i][money] = MAX(Comp[i].second.second - Comp[i].second.first + 
+			dp(money - Comp[i].first,i+1,p,Memo),dp(money,i+1,p,Memo));
+		}else{
+
+			Memo[i][money] = MAX(Comp[i].second.first - Comp[i].second.second + 
+			dp(money - Comp[i].first,i+1,p,Memo),dp(money,i+1,p,Memo));
 		}
 	}
+
 	return Memo[i][money];
 }
 int main() 
 { 
-	Adj g;
-	int n_pd, n_prism, n, money, i, j;
-	cin >> n_pd >> n_prism >> n >> money;
+	int politics_PD, politics_PRISM, number_discontent, initial_money;
+	int size_graph = 0, politic_pd, politic_prism,i,j;
 
-	int price_pd[n_pd], price_prism[n_prism];
-	
-	g.resize(n_pd+n_prism);
-	visited = (int*)calloc(n_pd+n_prism,sizeof(int));
+	cin >> politics_PD >> politics_PRISM >> number_discontent >> initial_money;
 
-	for (i = 0; i < n_pd; i++){
-		cin >> price_pd[i];
+	size_graph = politics_PRISM + politics_PD + 1;
+
+
+	visited = (int*)calloc(size_graph,sizeof(int));
+	price_suborn = (int*)calloc(size_graph-1,sizeof(int));
+	Graph graph_component(size_graph);
+
+	for (i = 0; i < politics_PRISM + politics_PD;++i){
+		cin >> price_suborn[i];
 	}
-	for (i = 0; i < n_prism; i++){
-		cin >> price_prism[i];
-	}
-	
-	int u, v;
 
-	for (i = 0; i < n; i++){
-		cin >> u >> v;
-		--u;
-		--v;
-
-		g[u].push_back(v+n_pd);
-		g[v+n_pd].push_back(u);
-	}
-	for (i = 0; i < (n_pd+n_prism); i++)
+	for (i = 0; i < number_discontent ; ++i)
 	{
-		money_comp = 0;
-		cont_prism = 0;
-		cont_pd = 0;
+		cin >> politic_pd >> politic_prism;
+		graph_component[politic_pd].push_back(politic_prism + politics_PD);
+		graph_component[politic_prism + politics_PD].push_back(politic_pd);	
+	}
 
-		if(visited[i] != 1){
-			dfs(g,i,n_pd,price_pd,price_prism);
-			components.push_back(make_pair(money_comp, make_pair(cont_prism,cont_pd)));
+	for (i = 1; i < graph_component.size() ; ++i)
+	{
+		pd_cont = prism_cont = value_component = 0;
+		if(!visited[i]){
+			dfs(graph_component,politics_PD,i);
+			Comp.push_back(make_pair(value_component,make_pair(pd_cont,prism_cont)));
 		}
 	}
-	
-	memset(Memo,-1,sizeof(Memo));
-	cout << dp(money,0,0);
 
-	return 0; 
+	int Memo[Comp.size()][10001];
+	memset(Memo,-1,sizeof(Memo));
+
+	cout << Comp.size() << initial_money << endl;
+	int answer = dp(initial_money,0,1,Memo);
+	cout << answer+politics_PD;
+	memset(Memo,-1,sizeof(Memo));
+	answer = dp(initial_money,0,0,Memo);
+	cout << " " << answer+politics_PRISM;
+
+	for (i=0;i<Comp.size();++i){
+		printf("%d %d %d\n", Comp[i].first, Comp[i].second.first,Comp[i].second.second);
+	}
+
+	cout << Comp.size();
+	return 0;
 } 
